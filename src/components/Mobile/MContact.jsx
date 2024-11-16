253b3b0009dccea47ef85fa046e9edf1cfc0ee12
aditya17092001 <adityaudata@gmail.com>
2024-11-17 00:22:28 +0530

    Removed lite mode in contact section

--- src/components/Mobile/MContact.jsx
+++ src/components/Mobile/MContact.jsx
@@ -88,7 +88,7 @@ const MContact = React.forwardRef((props, ref) => {
 
             <div className='w-full'>
                 <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-3">Your Message</label>
-                <textarea id="message" rows="4" className="block p-2.5 w-[80%] mx-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." required></textarea>
+                <textarea id="message" rows="4" className="block p-2.5 w-[80%] mx-auto text-sm text-white bg-gray-700 rounded-lg border border-gray-600 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." required ></textarea>
             </div>
 
                     <div className='text-center mt-5'>
